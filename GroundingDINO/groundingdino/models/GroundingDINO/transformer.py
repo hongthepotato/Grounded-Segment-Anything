5ec8e316e8de65908372034c8af3487dce1c4790
shen_hong <hshen17@fudan.edu.cn>
2025-11-21 16:27:20 +0800

    deprecated torch.cuda.amp to torch.amp

--- GroundingDINO/groundingdino/models/GroundingDINO/transformer.py
+++ GroundingDINO/groundingdino/models/GroundingDINO/transformer.py
@@ -860,7 +860,7 @@ class DeformableTransformerDecoderLayer(nn.Module):
         return tensor if pos is None else tensor + pos
 
     def forward_ffn(self, tgt):
-        with torch.cuda.amp.autocast(enabled=False):
+        with torch.amp.autocast(device_type='cuda', enabled=False):
             tgt2 = self.linear2(self.dropout3(self.activation(self.linear1(tgt))))
         tgt = tgt + self.dropout4(tgt2)
         tgt = self.norm3(tgt)
