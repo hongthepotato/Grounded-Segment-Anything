d6e7dc67bbf4f05628f2340bb357a18915995adc
shen_hong <hshen17@fudan.edu.cn>
2026-01-06 09:35:13 +0800

    internal api of online inference added

--- ml_engine/jobs/handlers/__init__.py
+++ ml_engine/jobs/handlers/__init__.py
@@ -7,10 +7,12 @@ Each handler implements the JobHandler interface and runs in an isolated subproc
 from ml_engine.jobs.handlers.base import JobHandler, TrainingCancelledError
 from ml_engine.jobs.handlers.teacher import TeacherTrainingHandler
 from ml_engine.jobs.handlers.auto_label import AutoLabelHandler
+from ml_engine.jobs.handlers.inference import InferenceHandler
 
 __all__ = [
     "JobHandler",
     "TrainingCancelledError",
     "TeacherTrainingHandler",
     "AutoLabelHandler",
+    "InferenceHandler",
 ]
