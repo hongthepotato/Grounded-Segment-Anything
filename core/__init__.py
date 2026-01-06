5f291f6bc510080b074dbd781ef1a54ea7d1c1c5
shen_hong <hshen17@fudan.edu.cn>
2026-01-06 15:31:22 +0800

    redundant method  removed

--- core/__init__.py
+++ core/__init__.py
@@ -1,14 +1,11 @@
 """Core configuration and utility modules."""
 
-from .config import load_config, save_config, generate_config
+from .config import load_config, save_config
 from .logger import setup_logger, get_logger
 
 __all__ = [
     'load_config',
     'save_config',
-    'generate_config',
     'setup_logger',
     'get_logger'
 ]
-
-
