4134c34124ef5c7a3aaf1f872309798fe1b39486
shen_hong <hshen17@fudan.edu.cn>
2026-03-26 13:42:11 +0800

    distillation package init file added

---
+++ ml_engine/distillation/__init__.py
@@ -0,0 +1,17 @@
+"""
+Knowledge distillation pipeline.
+
+Converts fine-tuned teacher knowledge into prompt-free student models
+through offline pseudo-labeling and ultralytics training.
+"""
+
+from ml_engine.distillation.pseudo_label import generate_pseudo_labels
+from ml_engine.distillation.utils import merge_coco_datasets, convert_coco_to_yolo_seg
+from ml_engine.distillation.student_trainer import StudentTrainer
+
+__all__ = [
+    "generate_pseudo_labels",
+    "merge_coco_datasets",
+    "convert_coco_to_yolo_seg",
+    "StudentTrainer",
+]
